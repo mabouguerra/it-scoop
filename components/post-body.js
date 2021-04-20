63ff397aefea8636f2f8b6d1acd4d3664764dc7a
Mohamed Amine Bouguerra <mabouguerra@gmail.com>
2021-04-20 22:45:21 +0100

    prose-lg for sm

--- components/post-body.js
+++ components/post-body.js
@@ -5,7 +5,7 @@ export default function PostBody({ content }) {
   return (
     <div className="mt-12 sm:mt-20 lg:mt-24 sm:pb-36 pb-28 lg:pb-40">
       <div
-        className="prose break-words prose-red sm:prose-sm lg:prose-lg max-w-none"
+        className="prose break-words prose-red sm:prose-lg max-w-none"
         dangerouslySetInnerHTML={{ __html: content }}
       ></div>
     </div>
