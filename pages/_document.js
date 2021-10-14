10c84b64cfb32f4e621c7261437d47fbf4125e72
mabouguerra <mabouguerra@gmail.com>
2021-10-14 11:39:56 +0200

    optimiezd cursive font

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ar" dir="rtl">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lalezar&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body className="overflow-x-hidden">
           <Main />
           <NextScript />
